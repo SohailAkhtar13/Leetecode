cbef18934a79be7b593b9b3e101de27b98fbd963
Casper <45950023+SohailAkhtar13@users.noreply.github.com>
2023-01-01 19:56:25 +0530

    Update twoSums.java

--- Arrays/twoSums.java
+++ Arrays/twoSums.java
@@ -1,14 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] ind = {0,0};
-        for (int i=0; i<nums.length-1; i++) {
-            for (int j=i+1; j<nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    ind [0] = i;
-                    ind [1] = j;
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    int a[]={i,j};
+                    return a;
                 }
             }
         }
-    return ind;
+        return null;
     }
 }
